14495c334e5fadc2a33ed95016bf1d458397bc4d
EaloEafit <lcealoo@eafit.edu.co>
2026-03-27 07:26:46 -0500

    Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(moveHorizontal, moveVertical, 0);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
