f7f5f022b079797b06918d1980194869bf154bd7
EaloEafit <lcealoo@eafit.edu.co>
2026-04-17 07:35:07 -0500

    Text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro; //
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,11 +7,13 @@ public class PlayerController : MonoBehaviour
     public int score = 0;
     public bool hasKey = false;
     public bool hasWater = false;
+    public TextMeshProUGUI textScore; //variable para conectar con nuestro Texto en la UI
+     public TextMeshProUGUI notificationText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       UpdateTextScore(); 
     }
 
     // Update is called once per frame
@@ -29,6 +32,9 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Collectable"))
         {
             score = score + 1;//le sumo 1 a la variable score
+            UpdateTextScore();
+            ShowNotification("Collected!");; 
+
             Destroy(other.gameObject);
             Debug.Log("Collected!");
             Debug.Log("Score: " + score);
@@ -37,6 +43,10 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Key"))
         {
             hasKey = true;
+            score = score + 5;
+            UpdateTextScore();
+            ShowNotification("KEY Collected!"); 
+
             Debug.Log("KEY Collected!");
             Destroy(other.gameObject);
         }
@@ -44,18 +54,30 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Water"))
         {
             hasWater = true;
+            ShowNotification("Has tocado el agua, no puedes ganar");
             Debug.Log("Has tocado el agua, comunicate con el servicio tecnico de Samsung, Oh!");
             
         }
                 
         if (score >= 3 && hasKey == true && !hasWater) //
         {
+            ShowNotification("You Won!");
             Debug.Log("You Won!");
         }
         /*else
         {
              Debug.Log("Keep trying, you need 3 and the KEY to win");
         }*/
+    }
+
+    void UpdateTextScore()
+    {
+         textScore.text = "Score: " + score;
+    }
+
+    void ShowNotification(string message)
+    {
+         notificationText.text = message; 
     }
 
 }
