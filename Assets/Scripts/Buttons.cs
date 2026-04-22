ba2fd5753e7646aeb98b9552e96c69c376d2d9e2
EaloEafit <lcealoo@eafit.edu.co>
2026-04-22 07:24:00 -0500

    Main Menu and Buttons

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Buttons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit ();
+    }
+
+
+}
